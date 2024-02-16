208def94f37a5cfa83198c5a9ffd0571f26e7237
MaximCore007 <malyshenko_mv@teslatec.ru>
2024-02-16 19:34:16 +0700

    Added sliders and buttons

--- main.py
+++ main.py
@@ -1,32 +1,86 @@
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtCore
+from PySide6.QtWidgets import QApplication, QMainWindow, QSlider, QBoxLayout, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QPushButton, QSpinBox, QFileDialog
 
-class MyWidget(QtWidgets.QWidget):
+class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        self.setWindowTitle("Slider Example")
+        self.setGeometry(100, 100, 400, 200)
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        main_layout = QVBoxLayout()
+        main_widget = QWidget()
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
+        
+        hue_layout = QHBoxLayout()
+        self.hue_slider = MyWindow.__createSlider("Hue", 0, 360, hue_layout)
+        main_layout.addLayout(hue_layout)
+        
+        saturation_layout = QHBoxLayout()
+        self.saturation_slider = MyWindow.__createSlider("Saturation", 0, 100, saturation_layout)
+        main_layout.addLayout(saturation_layout)
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        value_layout = QHBoxLayout()
+        self.value_slider = MyWindow.__createSlider("Saturation", 0, 100, value_layout)
+        main_layout.addLayout(value_layout)
+        
+        self.set_color_button = QPushButton("Set Color")
+        main_layout.addWidget(self.set_color_button)
+        self.set_color_button.clicked.connect(self.set_color)
 
-        self.button.clicked.connect(self.magic)
+        
+        self.select_file_label = QLabel("Open 'elf' file")
+        self.select_file_btn = QPushButton("Browse")
+        self.select_file_layout = QVBoxLayout()
+        self.select_file_layout.addWidget(self.select_file_label)
+        self.select_file_layout.addWidget(self.select_file_btn)
+        main_layout.insertLayout(0, self.select_file_layout)
+
+        self.set_color_button.clicked.connect(self.set_color)
+        self.select_file_btn.clicked.connect(self.open_file)
 
     @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+    def set_color(self):
+        print(f'Color(hue: {self.hue_slider.value()}, saturation: {self.saturation_slider.value()}, value: {self.value_slider.value()})')
+
+    @QtCore.Slot()
+    def open_file(self):
+        fileName = QFileDialog.getOpenFileName(self, str("Browser"), "~/", str("Image Files (*.txt)"))
+        name = ""
+        for file in fileName:
+            name += file
+        self.select_file_label.setText(fileName[0])
+
+    def __createSlider(NameSlider: str, min: int, max: int, layout: QBoxLayout) -> QSlider:
+        label = QLabel(NameSlider)
+        layout.addWidget(label)
+
+        value = QSpinBox()
+        value.setMinimum(min)
+        value.setMaximum(max)
+        value.setButtonSymbols(QSpinBox.ButtonSymbols.NoButtons)
+
+        slider = QSlider()
+        slider.setOrientation(QtCore.Qt.Orientation.Horizontal)  # Horizontal orientation
+        slider.setMinimum(min)
+        slider.setMaximum(max)
+        slider.setTickPosition(QSlider.TicksBelow)
+        slider.setTickInterval(10)
+        
+        slider.valueChanged.connect(value.setValue)
+        value.valueChanged.connect(slider.setValue)
+
+        layout.addWidget(slider)
+        layout.addWidget(value)
+
+        slider.setValue(0)  # Initial value
+
+        return slider
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
-
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
     sys.exit(app.exec())
